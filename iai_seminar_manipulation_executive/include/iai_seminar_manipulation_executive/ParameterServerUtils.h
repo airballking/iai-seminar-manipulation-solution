ea6e30251e6307d1c45f1959ae5d37b3209139a3
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2013-04-24 16:18:51 +0200

    Added utilities functions to load parameters from parameter server.

---
+++ iai_seminar_manipulation_executive/include/iai_seminar_manipulation_executive/ParameterServerUtils.h
@@ -0,0 +1,32 @@
+#include <ros/ros.h>
+#include <vector>
+
+/** Loads a vector of strings from the parameter server.
+
+    \param n [in] namespace to use for the lookup.
+    \param parameter_name [in] name of the parameter holding the vector.
+    \param parameters [out] container for loaded vector.
+    \return [out] true if everything worked out fine, else false.
+*/
+bool loadStringVectorFromParameterServer(ros::NodeHandle& n, const std::string& parameter_name,
+  std::vector<std::string>& parameters);
+
+/** Loads a vector of doubles from the parameter server.
+
+    \param n [in] namespace to use for the lookup.
+    \param parameter_name [in] name of the parameter holding the vector.
+    \param parameters [out] container for loaded vector.
+    \return [out] true if everything worked out fine, else false.
+*/
+bool loadDoubleVectorFromParameterServer(ros::NodeHandle& n, const std::string& parameter_name,
+  std::vector<double>& parameters);
+
+/** Loads a single double from the parameter server.
+
+    \param n [in] namespace to use for the lookup.
+    \param parameter_name [in] name of the parameter.
+    \param parameter [out] container for loaded double.
+    \return [out] true if everything worked out fine, else false.
+*/
+bool loadDoubleFromParameterServer(ros::NodeHandle& n, const std::string& parameter_name,
+  double& parameter);
---
+++ iai_seminar_manipulation_executive/src/ParameterServerUtils.cpp
@@ -0,0 +1,97 @@
+#include <iai_seminar_manipulation_executive/ParameterServerUtils.h>
+
+bool loadStringVectorFromParameterServer(ros::NodeHandle& n, const std::string& parameter_name,
+  std::vector<std::string>& parameters)
+{
+  XmlRpc::XmlRpcValue vector;
+  if(!n.getParam(parameter_name, vector))
+  {
+    ROS_ERROR("Parameter '%s' not given in namespace '%s'",
+      parameter_name.c_str(), n.getNamespace().c_str());
+    return false;
+  }
+
+  if(vector.getType() != XmlRpc::XmlRpcValue::TypeArray)
+  {
+    ROS_ERROR("Loaded parameter was not of type array: Parameter '%s' from namespace '%s'",
+      parameter_name.c_str(), n.getNamespace().c_str());
+    return false;
+  }
+
+  parameters.clear();
+  for(int i=0; i<vector.size(); i++)
+  {
+    XmlRpc::XmlRpcValue &entry = vector[i];
+    if(entry.getType() != XmlRpc::XmlRpcValue::TypeString)
+    {
+      ROS_ERROR("Entryi #%d in parameter was not of type string: Parameter '%s' from namespace '%s'", i,
+        parameter_name.c_str(), n.getNamespace().c_str());
+      return false;
+    }
+
+    parameters.push_back((std::string)entry);
+  }
+
+  // all went fine
+  return true;
+}
+
+bool loadDoubleVectorFromParameterServer(ros::NodeHandle& n, const std::string& parameter_name,
+  std::vector<double>& parameters)
+{
+  XmlRpc::XmlRpcValue vector;
+  if(!n.getParam(parameter_name, vector))
+  {
+    ROS_ERROR("Parameter '%s' not given in namespace '%s'",
+      parameter_name.c_str(), n.getNamespace().c_str());
+    return false;
+  }
+
+  if(vector.getType() != XmlRpc::XmlRpcValue::TypeArray)
+  {
+    ROS_ERROR("Loaded parameter was not of type array: Parameter '%s' from namespace '%s'",
+      parameter_name.c_str(), n.getNamespace().c_str());
+    return false;
+  }
+
+  parameters.clear();
+  for(int i=0; i<vector.size(); i++)
+  {
+    XmlRpc::XmlRpcValue &entry = vector[i];
+    if(entry.getType() != XmlRpc::XmlRpcValue::TypeDouble)
+    {
+      ROS_ERROR("Entryi #%d in parameter was not of type double: Parameter '%s' from namespace '%s'", i,
+        parameter_name.c_str(), n.getNamespace().c_str());
+      return false;
+    }
+
+    parameters.push_back((double)entry);
+  }
+
+  // all went fine
+  return true;
+}
+
+bool loadDoubleFromParameterServer(ros::NodeHandle& n, const std::string& parameter_name,
+  double& parameter)
+{
+  XmlRpc::XmlRpcValue param;
+  if(!n.getParam(parameter_name, param))
+  {
+    ROS_ERROR("Parameter '%s' not given in namespace '%s'",
+      parameter_name.c_str(), n.getNamespace().c_str());
+    return false;
+  }
+
+  if(param.getType() != XmlRpc::XmlRpcValue::TypeDouble)
+  {
+    ROS_ERROR("Loaded parameter was not of type double: Parameter '%s' from namespace '%s'",
+      parameter_name.c_str(), n.getNamespace().c_str());
+    return false;
+  }
+
+  parameter = (double) param;
+
+  // all went fine
+  return true;
+}
