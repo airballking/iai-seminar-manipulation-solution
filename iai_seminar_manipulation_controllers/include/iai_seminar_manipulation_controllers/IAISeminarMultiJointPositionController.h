cf58b3313f9f93b8a12c069f73aea524df754386
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2013-05-01 11:25:02 +0200

    Added initial empty version of joint-position controller.

---
+++ iai_seminar_manipulation_controllers/include/iai_seminar_manipulation_controllers/IAISeminarMultiJointPositionController.h
@@ -0,0 +1,14 @@
+#include <pr2_controller_interface/controller.h>
+#include <pr2_mechanism_model/joint.h>
+
+class IaiSeminarMultiJointPositionController: public pr2_controller_interface::Controller
+{
+private:
+  pr2_mechanism_model::JointState* joint_state_;
+
+public:
+  virtual bool init(pr2_mechanism_model::RobotState *robot, ros::NodeHandle& n);
+  virtual void starting();
+  virtual void update();
+  virtual void stopping();
+};
---
+++ iai_seminar_manipulation_controllers/src/IAISeminarMultiJointPositionController.cpp
@@ -0,0 +1,22 @@
+#include <iai_seminar_manipulation_controllers/IAISeminarMultiJointPositionController.h>
+#include <pluginlib/class_list_macros.h>
+
+bool IaiSeminarMultiJointPositionController::init(pr2_mechanism_model::RobotState *robot, ros::NodeHandle& n)
+{
+  return false;
+}
+
+void IaiSeminarMultiJointPositionController::starting()
+{
+
+}
+
+void IaiSeminarMultiJointPositionController::update()
+{
+
+}
+
+void IaiSeminarMultiJointPositionController::stopping()
+{
+
+}
